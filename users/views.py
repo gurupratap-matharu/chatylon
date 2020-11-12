a2623aee8d6fedc087f91b54f7b3bf5248331b56
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-12 10:25:01 -0300

    removed unused import

--- users/views.py
+++ users/views.py
@@ -1,12 +1,8 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView, DetailView, UpdateView
-from rest_framework import generics
 
 from users.models import Profile
-from users.permissions import IsAuthorOrReadOnly, IsProfileAuthorOrReadOnly
-from users.serializers import ProfileSerializer, UserSerializer
 
 
 class ProfileDetailView(LoginRequiredMixin, DetailView):
@@ -31,27 +27,3 @@ class ProfileUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     fields = ['bio', 'location', 'country', 'birth_date']
     template_name = 'users/profile_update_form.html'
     success_message = 'Profile updated successfully!'
-
-
-class UserListAPIView(generics.ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-    permissions_classes = (IsAuthorOrReadOnly,)
-
-
-class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-    permissions_classes = (IsAuthorOrReadOnly,)
-
-
-class ProfileListAPIView(generics.ListAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-    permission_classes = (IsProfileAuthorOrReadOnly,)
-
-
-class ProfileDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-    permission_classes = (IsProfileAuthorOrReadOnly,)
