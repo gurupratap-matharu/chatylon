ccccb5a46106a9bd79a132d58a3350e44ffc9b4d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-12 12:53:22 -0300

    home page list view with all users

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from users.models import CustomUser
+
+CustomUser = get_user_model()
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView, LoginRequiredMixin):
+    model = CustomUser
+    context_object_name = 'user_list'
     template_name = 'chat/home.html'
