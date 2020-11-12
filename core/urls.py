c22094c35c68919129d75850b3234dc1b5cbdf8b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-11 22:40:51 -0300

    added users app to root urlconf

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # django administration
+    path('dj-admin/', admin.site.urls),
+
+    # User management
+    path('accounts/', include('allauth.urls')),
+
+    # Local apps
+    path('profile/', include('users.urls')),
 ]
