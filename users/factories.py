a6b8c9936453c1420fd2a2ce4bf24e907d5faa03
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-11 22:47:11 -0300

    factories to generate mock data

---
+++ users/factories.py
@@ -0,0 +1,12 @@
+import factory
+from django.contrib.auth import get_user_model
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.Faker('email')
+    username = factory.Faker('user_name')
