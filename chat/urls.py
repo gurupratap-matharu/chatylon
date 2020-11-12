6b451a1cab80379e5563818116f2d893ded375e7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-12 16:51:20 -0300

    detail view for each room

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from chat.views import HomePageView
+from chat.views import ChatDetailView, HomePageView
 
 app_name = 'chat'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('<uuid:pk>/', ChatDetailView.as_view(), name='chat_detail'),
 ]
