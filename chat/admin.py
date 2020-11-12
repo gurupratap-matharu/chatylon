6c56f3cadac8a227ed9ade4d7507885ffbb6bf30
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-12 16:50:53 -0300

    added chat and chatroom to django admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat.models import Chat, ChatRoom
+
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('author', 'room', 'body', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('author', 'room')
+    date_hierarchy = 'created_on'
+    ordering = ('created_on',)
+    raw_id_fields = ('author',)
+
+
+class ChatInLine(admin.TabularInline):
+    model = Chat
+
+
+class ChatRoomAdmin(admin.ModelAdmin):
+    inlines = (ChatInLine, )
+    model = ChatRoom
+    list_display = ('name', 'active',)
+    list_filter = ('active', 'created_on',)
+    list_editable = ('active',)
+    search_fields = ('name', 'participants',)
+    raw_id_fields = ('participants',)
+    date_hierarchy = 'created_on'
+    ordering = ('active', 'created_on',)
+
+
+admin.site.register(ChatRoom, ChatRoomAdmin)
+admin.site.register(Chat, ChatAdmin)
