59ab51241b6ec0c306a85982939f66088d58996f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-12 14:32:38 -0300

    added heroku domain to allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['testserver', 'localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['testserver', 'localhost', '127.0.0.1', '0.0.0.0', 'chatylon.herokuapp.com']
 
 
 # Application definition
