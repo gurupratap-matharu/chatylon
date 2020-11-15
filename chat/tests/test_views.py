a91ec5f3343f3fa0e70e88080d25b25103ce072d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-15 18:41:08 -0300

    added unit test for home page

--- chat/tests/test_views.py
+++ chat/tests/test_views.py
@@ -1,17 +1,36 @@
 from chat.views import ChatDetailView, HomePageView
 from django.test import TestCase
 from django.urls import resolve, reverse
+from users.factories import UserFactory
 
 
 class HomePageTests(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+
     def test_home_page_resolves_homepageview(self):
-        pass
+        view = resolve(reverse('chat:home'))
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
 
     def test_home_page_works_for_logged_in_user(self):
-        pass
+        self.client.force_login(self.user)
+
+        response = self.client.get(reverse('chat:home'))
+        no_response = self.client.get('/homepage/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'chat/home.html')
+        self.assertContains(response, 'Contacts')
+        self.assertNotContains(response, 'Hi I should not be on this page!')
+        self.assertEqual(no_response.status_code, 404)
 
     def test_home_page_redirects_for_anonymous_user(self):
-        pass
+        response = self.client.get(reverse('chat:home'))
+        no_response = self.client.get('/homepage/')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateNotUsed(response, 'chat/home.html')
+        self.assertEqual(no_response.status_code, 404)
 
 
 class ChatPageTests(TestCase):
@@ -22,4 +41,7 @@ class ChatPageTests(TestCase):
         pass
 
     def test_chat_page_redirects_for_anonymous_user(self):
+        pass
+
+    def test_valid_post_data_adds_chat_to_chatroom(self):
         pass
